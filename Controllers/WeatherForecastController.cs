2317204996b7f10b33df0acd465856a6df425d80
Gokul <iamgokulnathanmcs@gmail.com>
2024-01-28 15:23:49 +0530

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cookurie.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+    }
+}
